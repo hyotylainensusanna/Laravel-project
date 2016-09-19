ed471e5c68db2df8a02e1919b1115fbe7ca0f226
Hyötyläinen,Susanna S.A.M <s.hyotylainen@student.fontys.nl>
2016-09-19 21:51:52 +0200

    added stylesheet and modified welcomepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,23 +7,39 @@
 
         <title>Laravel</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <!-- jQuery -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js"/>
 
         <!-- Latest compiled and minified CSS -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"/>
 
         <!-- Latest compiled and minified JavaScript -->
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
     </head>
     <body>
     <div class="container-fluid">
-        <header>
+        <header class="welcome-page-header">
             <h1>hello</h1>
         </header>
-        <section class="gategory"></section>
-        <section class="gategory"></section>
-        <section class="gategory"></section>
+        <section class="gategory">
+            <div class="row">
+                <div class="col-md-6">Gategory1</div>
+                <div class="col-md-3">place1</div>
+                <div class="col-md-3">place2</div>
+            </div>
+        </section>
+        <section class="gategory">
+            <div class="row">
+                <div class="col-lg-6">
+                    ssfk
+                </div>
+                <div class="col-lg-3">ddk</div>
+            </div>
+        </section>
+        <section class="gategory">Gategory3</section>
     </div>
     </body>
 </html>
